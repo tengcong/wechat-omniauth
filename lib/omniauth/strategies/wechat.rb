ae375372e45cf6b7a883d9dc18e320552c62ab4a
teng.cong <congteng45@gmail.com>
2014-10-16 14:03:24 +0800

    add debugger

--- lib/omniauth/strategies/wechat.rb
+++ lib/omniauth/strategies/wechat.rb
@@ -38,8 +38,18 @@ module OmniAuth
       def request_phase
         auth_hash = request.params['auth_hash']
         callback_url = "#{callback_url}?auth_hash=#{auth_hash}"
+
+        Rails.logger.info '-' * 30
+
+        Rails.logger.info request.params
+        Rails.logger.info callback_url
+        Rails.logger.info params
+
         params = client.auth_code.authorize_params.merge(redirect_uri: callback_url).merge(authorize_params)
         params["appid"] = params.delete("client_id")
+
+        Rails.logger.info params
+
         redirect client.authorize_url(params)
       end
 
